8463c4aa24cd8ab57f5af99d1e9f1a1976cca203
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-02 23:30:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'Male', 'Brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'Male', 'Black', 'agro', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'Female', 'Grizzly', 'agro', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'Male', 'Brown', 'behaved', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'Female', 'Black', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'Male', 'Black', 'agro', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'Black', 'lovely', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, 'Female', 'Black', 'calm', 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'Male', 'Brown', 'calm', 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'Male', 'Black', 'agro', 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'Female', 'Grizzly', 'agro', 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'Male', 'Brown', 'behaved', 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'Female', 'Black', 'calm', 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'Male', 'Black', 'agro', 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'Black', 'lovely', 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, 'Female', 'Black', 'calm', 1);
